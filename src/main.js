52efe2e85f1b64d70b08300fdf03fd1119f82849
sungg <zwsun1948@163.com>
2020-07-02 23:07:48 +0800

    删除vue相关

--- src/main.js
+++ src/main.js
@@ -1,44 +1,42 @@
 // 基于准备好的dom，初始化echarts实例
-const myChart = echarts.init(document.getElementById('skills'))
+const myChart = echarts.init(document.getElementById("skills"));
 
 const option = {
-
   tooltip: {
-    trigger: 'axis'
+    trigger: "axis",
   },
 
   radar: [
     {
       indicator: [
-        {text: '静态页面', max: 100},
-        {text: '编程基础', max: 100},
-        {text: 'React', max: 100},
-        {text: 'Vue', max: 100},
-        {text: '项目开发', max: 100},
-        {text: '沟通能力', max: 100}
+        { text: "静态页面", max: 100 },
+        { text: "编程基础", max: 100 },
+        { text: "React", max: 100 },
+        { text: "项目开发", max: 100 },
+        { text: "沟通能力", max: 100 },
       ],
-      center: ['50%', '50%'],
-      radius: 105
+      center: ["50%", "50%"],
+      radius: 105,
     },
   ],
   series: [
     {
-      type: 'radar',
+      type: "radar",
       tooltip: {
-        trigger: 'item'
+        trigger: "item",
       },
       areaStyle: {
-        color:'rgba(255,255,255,0.3)'
+        color: "rgba(255,255,255,0.3)",
       },
       data: [
         {
-          value: [100, 90, 85, 75,80,75],
-          name: '能力相对值'
-        }
-      ]
+          value: [100, 90, 85, 70, 75],
+          name: "能力相对值",
+        },
+      ],
     },
-  ]
+  ],
 };
 
 // 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option)
+myChart.setOption(option);
